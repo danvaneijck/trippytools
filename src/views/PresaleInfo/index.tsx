4eeb89f227411a832f067831d173e3918889c889
Daniel Van Eijck <dan.vaneijck@sensys.co.nz>
2024-04-06 21:46:18 +1300

    fix mobile

--- src/views/PresaleInfo/index.tsx
+++ src/views/PresaleInfo/index.tsx
@@ -25,7 +25,7 @@ const PreSaleInfo = () => {
     const divStyle = {
         position: 'relative',
         width: '100%',
-        paddingBottom: '25%',
+        paddingBottom: '5%',
     };
 
     const iframeStyle = {
@@ -34,18 +34,18 @@ const PreSaleInfo = () => {
         border: 0,
     };
 
-    return <div ref={myRef} className=''>
+    return <div ref={myRef} className='overflow-hidden'>
         <div>
-            <div className='text-center pt-8 text-5xl font-bold '>
-                $TRIPPY Pre Sale
+            <div className='text-center pt-8 text-4xl md:text-5xl font-bold '>
+                $TRIPPY
             </div>
-            <div className='text-center text-3xl '>
+            <div className='text-center text-xl md:text-3xl '>
                 on injective
             </div>
-            <div className='mt-2 text-center py-2 text-4xl w-1/5 m-auto rounded-xl p-2 mb-2 bg-gradient-to-br from-transparent to-white'>
-                date to be announced
+            <div className='mt-2 text-center py-2 text-2xl md:text-4xl md:w-1/3 m-auto rounded-xl p-2 mb-2 bg-gradient-to-br from-transparent to-black '>
+                pre sale date to be announced
             </div>
-            <div className='text-center  text-3xl font-bold'>
+            <div className='text-center  text-2xl md:text-3xl font-bold'>
                 supply: 1,000,000,000
                 <br />
                 max cap 2500 $INJ
@@ -54,7 +54,7 @@ const PreSaleInfo = () => {
                 <br />
                 min per wallet: 0.1 $INJ
             </div>
-            <div className='text-center pt-5 text-3xl font-bold'>
+            <div className='text-center pt-5 text-xl md:text-3xl font-bold'>
                 50 % LP (500,000,000)
                 <br />
                 49.5 % pre sale airdrop (495,000,000)
@@ -63,29 +63,29 @@ const PreSaleInfo = () => {
                 <br />
             </div>
 
-            <div className='mt-2 text-center py-5 text-5xl w-1/3 m-auto rounded-xl p-2 mb-2 bg-gradient-to-br from-transparent to-black'>
+            <div className='mt-2 text-center py-5 text-2xl md:text-5xl md:w-1/2  m-auto rounded-xl p-2 mb-2 bg-gradient-to-br from-transparent to-black'>
                 <a
                     className='hover:cursor-pointer hover:text-6xl'
                     href='https://coinhall.org/injective/inj1m35kyjuegq7ruwgx787xm53e5wfwu6n5uadurl'
                 >
                     $SHROOM
                 </a> multiplier 🔥
-                <div className='text-xl'>
+                <div className='text-sm md:text-xl'>
                     the more shrooms you eat, the more trippy you get
                 </div>
-                <div className='text-3xl'>
-                    send up to <span className='font-bold text-4xl'>10,000,000 $SHROOM</span> tokens <br /> with your $INJ to get a <span className='font-bold text-5xl'>1.25x</span> multiplier on your allocation
+                <div className='text-xl md: text-3xl'>
+                    send up to <span className='font-bold text-2xl md:text-4xl'>10,000,000 $SHROOM</span> tokens <br /> with your $INJ to get a <span className='font-bold text-3xl md:text-5xl'>1.25x</span> multiplier on your allocation
                 </div>
                 <div className='text-lg'>all shrooms sent in will be burned</div>
                 <div className='flex flex-row justify-center'>
                     <Link
                         to="/token-holders"
-                        className='text-lg hover:cursor-pointer hover:text-xl mr-10'
+                        className='text-sm md:text-lg hover:cursor-pointer hover:text-xl mr-10'
                     >
                         view $SHROOM holder distribution
                     </Link>
                     <a
-                        className=' text-lg hover:cursor-pointer hover:text-xl'
+                        className='text-sm md:text-lg hover:cursor-pointer hover:text-xl'
                         href='https://coinhall.org/injective/inj1m35kyjuegq7ruwgx787xm53e5wfwu6n5uadurl'
                     >
                         buy $SHROOM
@@ -97,7 +97,7 @@ const PreSaleInfo = () => {
                 id="dexscreener-embed"
                 // @ts-ignore
                 style={divStyle}
-                className='flex justify-center flex-row'
+                className='flex justify-center flex-row mx-2'
             >
                 <iframe
                     src="https://dexscreener.com/injective/inj1m35kyjuegq7ruwgx787xm53e5wfwu6n5uadurl?embed=1&theme=dark&trades=0"
