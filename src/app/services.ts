8b7e48cc4f5ead2cd9046fc87ea3afb19db8e67e
Daniel Van Eijck <dan.vaneijck@sensys.co.nz>
2024-04-10 23:59:22 +1200

    fix build

--- src/app/services.ts
+++ src/app/services.ts
@@ -1,11 +1,6 @@
 import { Network, getNetworkEndpoints } from "@injectivelabs/networks";
-import { MsgBroadcaster } from "@injectivelabs/wallet-ts";
-import { walletStrategy } from "./services/wallet";
+// import { MsgBroadcaster } from "@injectivelabs/wallet-ts";
+// import { walletStrategy } from "./services/wallet";
 
-export const NETWORK = Network.TestnetK8s;
+export const NETWORK = Network.Mainnet;
 export const ENDPOINTS = getNetworkEndpoints(NETWORK);
-
-export const msgBroadcastClient = new MsgBroadcaster({
-  walletStrategy,
-  network: NETWORK,
-});
