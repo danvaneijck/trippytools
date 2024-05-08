4602270ea302d4f48bc549bf585607fc8041f0fc
dan <dan.vaneijck@sensys.co.nz>
2024-05-08 17:23:13 +1200

    add shroom spore pool

--- src/constants/walletLabels.ts
+++ src/constants/walletLabels.ts
@@ -4,6 +4,7 @@ const trippykiwiAddress = "inj1lq9wn94d49tt7gc834cxkm0j5kwlwu4gm65lhe"
 const rugLord = "inj1nsqdlh065qveeqkeh64lhap3hm40ptrt8t2unw"
 const shroomPool = "inj1m35kyjuegq7ruwgx787xm53e5wfwu6n5uadurl"
 const shroomMarketingWallet = "inj1pr5lyuez8ak94tpuz9fs7dkpst7pkc9uuhfhvm"
+const sporeShroomPool = "inj1rusfnzgtcvkn8z92h9hyvzuna60tc0x0yy74tf"
 
 export const walletLabels = {
     [rugLord]: {
@@ -33,6 +34,11 @@ export const walletLabels = {
     },
     [shroomPool]: {
         label: "shroom pool",
+        bgColor: "",
+        textColor: "text-green-500"
+    },
+    [sporeShroomPool]: {
+        label: "spore/shroom pool",
         bgColor: "",
         textColor: "text-green-500"
     }
