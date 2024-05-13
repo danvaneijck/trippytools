500a0056c3fb81d90b61c7122f07e23b743ea6ce
dan <dan.vaneijck@sensys.co.nz>
2024-05-13 18:13:00 +1200

    add more nft collections

--- src/constants/contractAddresses.ts
+++ src/constants/contractAddresses.ts
@@ -169,5 +169,40 @@ export const NFT_COLLECTIONS = [
         value: "inj19syt7zhn4ytvf7xtpw8wml558hz5hh5yjjrszn",
         label: "Goblin Royale",
         img: "https://talis-protocol.mo.cloudinary.net/inj/families/65a9700d15d89b465b3f7e1b/miniaturePicture"
+    },
+    {
+        value: "inj1e34fuwkxxdz8ve8n7h92jdk8kd68wxxc0waztg",
+        label: "INJECTIVE CLUB",
+        img: "https://talis-protocol.mo.cloudinary.net/inj/families/6633e4e8fe915c81f644c6cc/miniaturePicture"
+    },
+    {
+        value: "inj1n73yuus64z0yrda9hvn77twkspc4uste9j9ydd",
+        label: "Sushi Fighters",
+        img: "https://talis-protocol.mo.cloudinary.net/inj/families/65d7825fefc5876b5878b430/miniaturePicture"
+    },
+    {
+        value: "inj1p0kdm00l0j0mk7r7nt6ufkhj2cg8yxqwtrkpm3",
+        label: "Destiny Punks",
+        img: "https://talis-protocol.mo.cloudinary.net/inj/families/6594acfc7789111dab2e5fb3/miniaturePicture"
+    },
+    {
+        value: "inj1lcep44ttk8tra6xc98eh0exlhf7774cjueju8l",
+        label: "Ninja Shuttles",
+        img: "https://talis-protocol.mo.cloudinary.net/inj/families/660564cfea3abedcf300c100/miniaturePicture"
+    },
+    {
+        value: "inj17jmvd3nm2nj84pmffu4p6sxfw2mqs5j4d7aa7n",
+        label: "$SNAPPY NFT Collection",
+        img: "https://talis-protocol.mo.cloudinary.net/inj/families/663dfdffa7369a8d6824f086/miniaturePicture"
+    },
+    {
+        value: "inj1dmn43jyt6w6kycgsuu5a3ygtmtk6fm49yvf73d",
+        label: "Panda Moonshot",
+        img: "https://talis-protocol.mo.cloudinary.net/inj/families/66170251b6b99c609875acf0/miniaturePicture"
+    },
+    {
+        value: "inj1al52xc734m23xxjlee28m9mjjcpfuen38che4q",
+        label: "NAPEJAS",
+        img: "https://talis-protocol.mo.cloudinary.net/inj/families/65e75806add77a2a935e81e2/miniaturePicture"
     }
 ]
