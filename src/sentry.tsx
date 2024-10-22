4cd500bb8929a07b9a49d8b85d5ad5a637c8e101
dan <dan.ve16587@gmail.com>
2024-10-22 21:44:28 +1300

    adjust sentry

--- src/sentry.tsx
+++ src/sentry.tsx
@@ -7,30 +7,21 @@ import {
     useNavigationType,
 } from "react-router-dom";
 
-Sentry.init({
-    dsn: "https://069c2c3bdf42a31e04ac86bdfde844af@o4508164578213888.ingest.us.sentry.io/4508164581163008",
-    integrations: [
-        // See docs for support of different versions of variation of react router
-        // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
-        Sentry.reactRouterV6BrowserTracingIntegration({
-            useEffect,
-            useLocation,
-            useNavigationType,
-            createRoutesFromChildren,
-            matchRoutes,
-        }),
-        Sentry.replayIntegration(),
-    ],
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for tracing.
-    tracesSampleRate: 1.0,
-
-    // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
-    //   tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
-
-    // Capture Replay for 10% of all sessions,
-    // plus for 100% of sessions with an error
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0,
-});
+if (process.env.NODE_ENV === 'production') {
+    Sentry.init({
+        dsn: "https://069c2c3bdf42a31e04ac86bdfde844af@o4508164578213888.ingest.us.sentry.io/4508164581163008",
+        integrations: [
+            Sentry.reactRouterV6BrowserTracingIntegration({
+                useEffect,
+                useLocation,
+                useNavigationType,
+                createRoutesFromChildren,
+                matchRoutes,
+            }),
+            Sentry.replayIntegration(),
+        ],
+        tracesSampleRate: 1.0, // Reduce this in development, or set based on the environment
+        replaysSessionSampleRate: 0.1, // Capture Replay for 10% of sessions
+        replaysOnErrorSampleRate: 1.0, // Capture Replay for 100% of sessions with errors
+    });
+}
